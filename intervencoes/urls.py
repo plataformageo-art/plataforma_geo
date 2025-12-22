9f3d0543e401631aac411d481de5e336c2a376f4
Rudolf Neubert <noreply.plataformageo@gmail.com>
2025-12-22 08:19:54 -0300

    Fix root URL conflict and clean URL configuration

--- intervencoes/urls.py
+++ intervencoes/urls.py
@@ -2,29 +2,29 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 from django.contrib.auth import views as auth_views
-from .views import IntervencaoViewSet
-from .views import api_intervencoes
+from .views import IntervencaoViewSet, api_intervencoes
 from django.urls import reverse_lazy
 
 router = DefaultRouter()
 router.register(r'comentarios', views.ComentarioViewSet)
 router.register(r'intervencoes', IntervencaoViewSet, basename='intervencoes')
 
-urlpatterns = router.urls
-
 urlpatterns = [
     path('', views.pagina_inicial, name='pagina_inicial'),
     path('cadastro/', views.pagina_cadastro, name='cadastro'),
     path('login/', views.login_view, name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(template_name='logged_out.html'), name='logout'),
-    # Somente a API REST
+
+    # API
     path('api/', include(router.urls)),
-    path("projeto/", views.projeto_view, name="projeto"),
     path('intervencoes/', api_intervencoes, name='api_intervencoes'),
+
+    path("projeto/", views.projeto_view, name="projeto"),
+
     path('password_reset/',
          auth_views.PasswordResetView.as_view(
              template_name='password_reset_form.html',
-             success_url=reverse_lazy('password_reset_done')  # 👈 redireciona para a rota correta
+             success_url=reverse_lazy('password_reset_done')
          ),
          name='password_reset'),
 
